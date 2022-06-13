a1c7ae133d4bf30228140ae55f04d6517d8582bb
Alex Pletta <alexpletta@gmail.com>
2022-06-13 13:09:11 -0400

    baseline tutorial

---
+++ Ref/SignalGen/SignalGenComponentImpl.cpp
@@ -0,0 +1,91 @@
+// ======================================================================
+// \title  SignalGen.cpp
+// \author alexpletta
+// \brief  cpp file for SignalGen component implementation class
+// ======================================================================
+
+
+#include <Ref/SignalGen/SignalGen.hpp>
+#include "Fw/Types/BasicTypes.hpp"
+
+namespace Ref {
+
+  // ----------------------------------------------------------------------
+  // Construction, initialization, and destruction
+  // ----------------------------------------------------------------------
+
+  SignalGen ::
+    SignalGen(
+        const char *const compName
+    ) : SignalGenComponentBase(compName)
+  {
+
+  }
+
+  void SignalGen ::
+    init(
+        const NATIVE_INT_TYPE queueDepth,
+        const NATIVE_INT_TYPE instance
+    )
+  {
+    SignalGenComponentBase::init(queueDepth, instance);
+  }
+
+  SignalGen ::
+    ~SignalGen()
+  {
+
+  }
+
+  // ----------------------------------------------------------------------
+  // Handler implementations for user-defined typed input ports
+  // ----------------------------------------------------------------------
+
+  void SignalGen ::
+    schedIn_handler(
+        const NATIVE_INT_TYPE portNum,
+        NATIVE_UINT_TYPE context
+    )
+  {
+    // TODO
+  }
+
+  // ----------------------------------------------------------------------
+  // Command handler implementations
+  // ----------------------------------------------------------------------
+
+  void SignalGen ::
+    SignalGen_Settings_cmdHandler(
+        const FwOpcodeType opCode,
+        const U32 cmdSeq,
+        U32 Frequency,
+        F32 Amplitude,
+        F32 Phase,
+        Ref::SignalType SigType
+    )
+  {
+    // TODO
+    this->cmdResponse_out(opCode,cmdSeq,Fw::CmdResponse::OK);
+  }
+
+  void SignalGen ::
+    SignalGen_Toggle_cmdHandler(
+        const FwOpcodeType opCode,
+        const U32 cmdSeq
+    )
+  {
+    // TODO
+    this->cmdResponse_out(opCode,cmdSeq,Fw::CmdResponse::OK);
+  }
+
+  void SignalGen ::
+    SignalGen_Skip_cmdHandler(
+        const FwOpcodeType opCode,
+        const U32 cmdSeq
+    )
+  {
+    // TODO
+    this->cmdResponse_out(opCode,cmdSeq,Fw::CmdResponse::OK);
+  }
+
+} // end namespace Ref
---
+++ Ref/SignalGen/SignalGenComponentImpl.hpp
@@ -0,0 +1,92 @@
+// ======================================================================
+// \title  SignalGen.hpp
+// \author alexpletta
+// \brief  hpp file for SignalGen component implementation class
+// ======================================================================
+
+#ifndef SignalGen_HPP
+#define SignalGen_HPP
+
+#include "Ref/SignalGen/SignalGenComponentAc.hpp"
+
+namespace Ref {
+
+  class SignalGen :
+    public SignalGenComponentBase
+  {
+
+    public:
+
+      // ----------------------------------------------------------------------
+      // Construction, initialization, and destruction
+      // ----------------------------------------------------------------------
+
+      //! Construct object SignalGen
+      //!
+      SignalGen(
+          const char *const compName /*!< The component name*/
+      );
+
+      //! Initialize object SignalGen
+      //!
+      void init(
+          const NATIVE_INT_TYPE queueDepth, /*!< The queue depth*/
+          const NATIVE_INT_TYPE instance = 0 /*!< The instance number*/
+      );
+
+      //! Destroy object SignalGen
+      //!
+      ~SignalGen();
+
+    PRIVATE:
+
+      // ----------------------------------------------------------------------
+      // Handler implementations for user-defined typed input ports
+      // ----------------------------------------------------------------------
+
+      //! Handler implementation for schedIn
+      //!
+      void schedIn_handler(
+          const NATIVE_INT_TYPE portNum, /*!< The port number*/
+          NATIVE_UINT_TYPE context /*!< 
+      The call order
+      */
+      );
+
+    PRIVATE:
+
+      // ----------------------------------------------------------------------
+      // Command handler implementations
+      // ----------------------------------------------------------------------
+
+      //! Implementation for SignalGen_Settings command handler
+      //! Signal Generator Settings
+      void SignalGen_Settings_cmdHandler(
+          const FwOpcodeType opCode, /*!< The opcode*/
+          const U32 cmdSeq, /*!< The command sequence number*/
+          U32 Frequency, 
+          F32 Amplitude, 
+          F32 Phase, 
+          Ref::SignalType SigType 
+      );
+
+      //! Implementation for SignalGen_Toggle command handler
+      //! Toggle Signal Generator On/Off.
+      void SignalGen_Toggle_cmdHandler(
+          const FwOpcodeType opCode, /*!< The opcode*/
+          const U32 cmdSeq /*!< The command sequence number*/
+      );
+
+      //! Implementation for SignalGen_Skip command handler
+      //! Skip next sample
+      void SignalGen_Skip_cmdHandler(
+          const FwOpcodeType opCode, /*!< The opcode*/
+          const U32 cmdSeq /*!< The command sequence number*/
+      );
+
+
+    };
+
+} // end namespace Ref
+
+#endif
